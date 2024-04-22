81314599e9d8198c5c031a276f7af265f89f872d
kyn <rladudshka@naver.com>
2024-04-22 14:54:12 +0900

    axios를 통한 GET요청추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <a href="http://localhost:3000/">로그인</a>
+    <a href="http://localhost:3000">로그인</a>
+    <v-btn @click="test">테스트</v-btn>
   </div> 
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
 
@@ -13,5 +15,22 @@ export default {
     //
     title : "안녕하세요"
   }),
+  methods: {
+    test() {
+         axios.get('http://localhost:3000')
+          // axios.get('https://reqres.in/api/users?page=2')
+            .then(res => {
+              // handle success
+              console.log(res);
+            })
+            .catch(err=> {
+              // handle error
+              console.log(err);
+            })
+            .finally(function () {
+              // always executed
+            });
+     }
+  }
 };
 </script>
