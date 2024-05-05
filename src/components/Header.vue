ef037e8132a59c8e82e309d24ede262991d5958e
kyn <rladudshka@naver.com>
2024-05-06 00:57:36 +0900

    아이콘이 포함된 버튼 음영수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,26 @@
 <template>
   <v-app-bar app>
     <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none"></v-app-bar-nav-icon>
-    <v-toolbar-title>짱!마트-잡화점</v-toolbar-title>
+    <v-toolbar-title>Y마트-잡화점</v-toolbar-title>
     <v-spacer></v-spacer>
     <category-menu></category-menu>
     <v-spacer></v-spacer>
     <v-btn text>로그인</v-btn>
     <v-btn text>회원가입</v-btn>
-    <v-btn left :to="{ name: 'mypage' }" style="box-shadow: none !important;">
+    <v-btn left :to="{ name: 'mypage' }" elevation="0">
       <v-icon>mdi-account</v-icon>마이페이지
     </v-btn>
-    <v-btn left :to="{ name: 'cart' }">
+    <v-btn left :to="{ name: 'cart' }" elevation="0">
       <v-icon>mdi-cart</v-icon>장바구니
     </v-btn>
   </v-app-bar>
 </template>
-
+<style scoped>
+ .v-btn:before {
+  display: none;
+}
+ 
+</style>
 <script>
 import CategoryMenu from '@/components/CategoryMenu'
 
