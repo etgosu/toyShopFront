883227639aa43608fe099fb93de57e6058308eb1
kyn <rladudshka@naver.com>
2024-05-08 13:57:49 +0900

    장바구니 리스트 구현 [상품조회 DB연동]

--- src/views/ShopBasket.vue
+++ src/views/ShopBasket.vue
@@ -1,3 +1,84 @@
 <template>
-  <div>장바구니</div>
+  <v-container>
+    <v-data-table :headers="headers" :items="numberedItems" item-key="_id">
+      <template v-slot:[`item.prodInfo.prodNm`]="{ item }">
+        <router-link :to="`/product/${item._id}`">{{
+          item.prodInfo.prodNm
+        }}</router-link>
+      </template>
+      <template v-slot:[`item.주문관리`]="{ item }">
+        <v-btn @click="purchase(item)">주문하기</v-btn>
+        <v-btn @click="cancel(item)">취소</v-btn>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
+
+<style>
+/* headers 가운데 정렬 */
+/* 헤더 가운데 정렬 */
+.v-data-table-header .v-data-table-header-wrapper {
+  justify-content: center;
+}
+
+/* v-btn 간격 조정 */
+.v-btn {
+  margin: 0 5px; /* 좌우 간격 조정 */
+}
+
+/* v-header에 음영 추가 */
+.v-data-table-header {
+  background-color: #f2f2f2; /* 배경색 지정 */
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "번호", value: "no" },
+        { text: "상품명", value: "prodInfo.prodNm" },
+        { text: "상품가격", value: "prodInfo.price" },
+        { text: "수량", value: "qty" },
+        { text: "주문관리", value: "주문관리" },
+      ],
+      baskets: [],
+    };
+  },
+  mounted() {
+    this.fetchBaskets();
+  },
+  computed: {
+    numberedItems() {
+      return this.baskets.map((item, index) => {
+        return {
+          ...item,
+          no: index + 1,
+        };
+      });
+    },
+  },
+  methods: {
+    fetchBaskets() {
+      axios
+        .get("http://localhost:3000/basket/list?userNm=김영남")
+        .then((response) => {
+          console.log(response);
+          this.baskets = response.data.basketList;
+        })
+        .catch((error) => {
+          console.error("Error !!!", error);
+        });
+    },
+    purchase(item) {
+      // 구매 동작
+    },
+    cancel(item) {
+      // 취소 동작
+    },
+  },
+};
+</script>
